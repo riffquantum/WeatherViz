b99c0f4d0894916029bb43342076f98c37975826
Quinn <qmm25@drexel.edu>
2017-10-02 15:59:33 -0400

    first files upload

---
+++ Assets/Scripts/LabelItems.js
@@ -0,0 +1,64 @@
+﻿#pragma strict
+
+//Option for Mouseover Tet Label
+
+var newTextMesh : TextMesh;	//textmesh to instantiate
+internal var myLabel : TextMesh;	//name of instantiated text
+internal var myText : String = "Hello";	//the text that will be displayed
+
+//Other visual feedback on mouseover
+//var scaleChange : float = 0.5;	//increase size of marker on mouseover
+//var rotationSpeed : float = 120;	//Rotate marker on mouseover
+//var highlightColor : Vector4 = Vector4(1, .8, .8, 1);	//change RGBA
+
+//By finding the Main Cam we can make the text always point at it to be readable
+internal var target : Transform;	//create transform var
+target = GameObject.Find("Main Camera").transform;	// assign the main cam to do it
+
+//Vars to keep track of old color/scale/etc to reset on mouse out
+//var oldColor : Color;
+//internal var oldOri : Vector3;
+
+//OnMouseEnter events trigger ONCE
+function OnPointerEnter(){
+	//Instantiate new 3D text obj
+	myLabel = Instantiate( newTextMesh,  new Vector3(transform.position.x+1, transform.position.y+1, transform.position.z+1), Quaternion.identity );
+	
+	myLabel.text = myText;	//change the text of the message
+	myLabel.transform.LookAt(target);	//rotate text to face camera
+	myLabel.transform.Rotate( Vector3(0,180,0));	//flip it 180 degrees
+	
+	//Add addtl visual feedback
+	//gameObject.transform.localScale += Vector3(scaleChange,scaleChange,scaleChange);	//make obj bigger
+	//oldColor = transform.GetComponent.<Renderer>().material.GetColor("Color");	//Store original color
+	//transform.GetComponent.<Renderer>().material.color = Vector4(10, .8, .8,1);	//change color
+	//transform.renderer.material.color = Color.white; //alt color change
+	//oldOri = transform.eulerAngles;
+}
+
+//OnMouseOver triggered everyfram while mouse hovers datapoint
+function OnMouseOver(){
+	
+	//more visual feedback
+	//transform.Rotate(Vector3( 0, rotationSpeed*Time.deltaTime, 0)); //rotate 3d marker
+
+}//end OnMOuseOver
+
+//OnMOuseExit triggered once
+function OnMouseExit(){
+	//Reset label
+	Destroy(myLabel);	//Delete the label when the mouse leaves
+	//reset scale,color,etc
+	//gameObject.transform.localScale -= Vector3(scaleChange, scaleChange, scaleChange);
+	//transform.GetComponent.<Renderer>().material.color = Vector4(0,1,1,1);
+	//transform.eulerAngles = oldOri;
+	
+	}//end OnMouseExit
+	
+	
+//Other scripts can call this function to change the label text
+function SetText( txt : String ){
+	myText = txt;
+}
+
+
---
+++ Assets/Scripts/ReadTextScript.js
@@ -0,0 +1,76 @@
+﻿#pragma strict
+
+
+var theSourceFile : TextAsset;	//var to set which txt file
+var marker : Transform;			//what 3D object will mark a datapoints pos
+var xColumn : float;
+var yColumn : float;
+var zColumn : float;
+var labelColumn : float;		//data column that displays on mouseover
+var windDir : float;
+var arrowRotation = Quaternion.identity;  //rotation from wind direction column
+var arrowScale : float;		//size of arrow from mean wind speed
+
+
+//Rescale data values to match a desired range of space
+var xMinMax : Vector2 = Vector2(0,100);	//smallest / largest values for x in dataset
+var yMinMax : Vector2 = Vector2(0,100);	//smallest / largest values for y in dataset
+var zMinMax : Vector2 = Vector2(0,100);	//smallest / largest values for z in dataset
+
+var windMinMax : Vector2 = Vector2(0,360); //smallest / largest values for windDir in dataset
+
+var axesMinMax : Vector2 = Vector2(0,100);	//smallest/largest virtual units for display
+
+
+
+function Start () {
+
+	var myText = theSourceFile.text;	//read all text from file into one long string
+	var myList = myText.Split("#"[0]);	//split text into lines
+	for (var i=1; i<myList.length; i++) {		//cycle thru each line, skip first myList.length
+		var dataList = myList[i].Split("\t"[0]);	//split each line into columns
+		
+		//for (var j=0; j<dataList.Length; j++){
+		//	print (dataList(j));
+		//	}
+		//print (myList[i].ToString() );		//all of this for debugging purposes
+
+		if (dataList.Length > 1){		//only process lines with content
+		var x = parseFloat( dataList[xColumn] );		//read out x - MeanTemp
+		var y = parseFloat( dataList[yColumn] );		//read out y - MeanDewPoint
+		var z = parseFloat( dataList[zColumn] );		//read out z - MeanHumdity
+		
+		var w = parseFloat( dataList[windDir] );		//read out Win Direction
+		var s = parseFloat( dataList[arrowScale] );		//read out Mean Wind Speed
+		
+		var myLabel : String = dataList[labelColumn];	//read out label text
+		
+		//scale variables to fit the desired range of virtual space
+		var xPct : float = (x-xMinMax[0] / (xMinMax[1] - xMinMax[0]));	//value-min / range = %
+		x = (xPct * (axesMinMax[1] - axesMinMax[0])) + axesMinMax[0];	//x * %, + min
+		print(y); print (yMinMax[1] - yMinMax[0]);
+		var yPct : float = (y-yMinMax[0] / (yMinMax[1] - yMinMax[0]));	//value-min / range = %
+		y = (yPct * (axesMinMax[1] - axesMinMax[0])) + axesMinMax[0];	//y * %, + min
+		print (yPct);
+		var zPct : float = (z-zMinMax[0] / (zMinMax[1] - zMinMax[0]));	//value-min / range = %
+		z = (zPct * (axesMinMax[1] - axesMinMax[0])) + axesMinMax[0];	//w * %, + min
+		//var wPct : float = (w-windMinMax[0] / (windMinMax[1] - windMinMax[0]));	//value-min / range = %
+		//w = (wPct * (axesMinMax[1] - axesMinMax[0])) + axesMinMax[0];	//w * %, + min
+		
+		
+		//set arrow rotation
+		arrowRotation.eulerAngles = Vector3(90,w,0);
+		 
+		//Use Instantiate to make a copy of the 3D marker at the desired location
+		var myMarker : Transform = Instantiate( marker, Vector3(x,y,z), arrowRotation); //
+		myMarker.transform.localScale = new Vector3(s*20,s*20,s*20);		//change Scale of instantiated prefab
+		
+		
+		//Send a message to the marker's LabelItems script, calling the SetText function 
+		myMarker.SendMessage("SetText", myLabel, SendMessageOptions.DontRequireReceiver );
+		
+		}	//end if statement
+	}	//end for loop
+
+}	//end of Start
+
